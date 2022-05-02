88e2028187d8d932a522a6cbf90b6b64e869493a
HazemLenin <hazemlenin@gmail.com>
2022-05-02 09:51:43 +0200

    changed library ui

--- db/migrate/20220419155436_add_book_statuses_count_to_book.rb
+++ db/migrate/20220419155436_add_book_statuses_count_to_book.rb
@@ -1,5 +1,5 @@
 class AddBookStatusesCountToBook < ActiveRecord::Migration[7.0]
   def change
-    add_column :books, :book_statuses_count, :integer
+    add_column :books, :book_statuses_count, :integer, default: 1
   end
 end
